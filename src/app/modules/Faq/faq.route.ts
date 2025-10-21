a1368fea76dde828e8bf8515dfddb643a505ae25
Moshfiqur Rahman <studentemam@gmail.com>
2025-10-21 16:14:41 +0600

    feat: add faq routes

---
+++ src/app/modules/Faq/faq.route.ts
@@ -0,0 +1,31 @@
+import express from "express";
+
+import { FaqController } from "./faq.controller";
+
+import validateRequest from "../../middlewares/validateRequest";
+import auth from "../../middlewares/auth";
+import { FaqValidation } from "./faq.validation";
+import { USER_ROLE } from "../User/user.constant";
+const router = express.Router();
+
+router
+    .route("/")
+    .post(
+        validateRequest(FaqValidation.createFaqZodSchema),
+        auth(USER_ROLE.admin, USER_ROLE.user),
+        FaqController.createFaq,
+    )
+    .get(FaqController.getFaqs);
+
+router
+    .route("/:id")
+    .delete(
+        auth(USER_ROLE.admin, USER_ROLE.user),
+        FaqController.deleteFaq,
+    )
+    .patch(
+        auth(USER_ROLE.admin, USER_ROLE.user),
+        FaqController.updateFaq,
+    );
+
+export const FaqRoutes = router;
