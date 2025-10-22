43faed204562dc18a19e80c1c443a626e01b932e
Moshfiqur Rahman <studentemam@gmail.com>
2025-10-22 11:49:30 +0600

    feat: add getAllBlogs controller

--- src/app/modules/Blog/blog.controller.ts
+++ src/app/modules/Blog/blog.controller.ts
@@ -9,10 +9,21 @@ const createBlog = catchAsync(async (req, res) => {
         success: true,
         statusCode: 200,
         message: "Blog data is created successfully",
-        data:result,
+        data: result,
     })
 });
 
-export const BlogControllers={
+const getAllBLogs = catchAsync(async (req, res) => {
+    const result = await BlogServices.getAllBLogsFromDB();
+    sendResponse(res, {
+        success: true,
+        statusCode: 200,
+        message: "Successfully retrieved blogs data",
+        data: result,
+    })
+})
+
+export const BlogControllers = {
     createBlog,
+    getAllBLogs,
 }
