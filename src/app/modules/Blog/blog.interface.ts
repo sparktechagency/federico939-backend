f1f7eec29a3bfd892f61b33c5ec8f75bad9faf27
Moshfiqur Rahman <studentemam@gmail.com>
2025-10-22 10:25:25 +0600

    feat: add TBlog interface

---
+++ src/app/modules/Blog/blog.interface.ts
@@ -0,0 +1,11 @@
+import { BLOG_CATEGORY } from "./blog.constant";
+
+export interface TBlog {
+    title: string;
+    description: string;
+    category: BLOG_CATEGORY;
+    thumbnail: string;
+    authorName: string;
+    authorImage: string;
+    blogLink:string;
+}
